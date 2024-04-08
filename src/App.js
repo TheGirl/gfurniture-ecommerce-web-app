c31857c6250b8a4994b84196bdd7bb713ea131d7
Muhammed <oparemimuhammedkolapo@gmail.com>
2024-04-07 18:08:06 -0700

    First Commit After Initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Home, Category, ProductDetails, Cart, Checkout } from './components';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { CartProvider } from './CartContext';
+import Success from './Pages/Success/Success';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <ToastContainer />
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route path='/category' element={<Category/>}></Route>
+          <Route path='/productdetails/:productId' element={<ProductDetails/>}></Route>
+          <Route path='/cart' element={<Cart/>}></Route>
+          <Route path='/checkout' element={<Checkout/>}></Route>
+          <Route path='/success' element={<Success/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
